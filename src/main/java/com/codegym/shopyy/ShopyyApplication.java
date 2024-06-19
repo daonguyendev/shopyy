8f2abfa95ca5ea4dee6a57e6eb1d00e86195cce8
daonguyendev <daonguyen.dev@gmail.com>
2024-06-19 09:28:28 +0700

    Init shopyy project

---
+++ src/main/java/com/codegym/shopyy/ShopyyApplication.java
@@ -0,0 +1,13 @@
+package com.codegym.shopyy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopyyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShopyyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codegym/shopyy/ShopyyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codegym.shopyy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopyyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
