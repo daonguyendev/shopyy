097e50a6d6674887892bb31f37a4efa301294e89
HuuTri331 <nguyenhuutri331@gmail.com>
2024-07-08 17:33:42 +0700

    refactor codes

--- src/main/java/com/codegym/shopyy/repository/IProductDetailRepository.java
+++ src/main/java/com/codegym/shopyy/repository/IProductDetailRepository.java
@@ -2,14 +2,10 @@ package com.codegym.shopyy.repository;
 
 import com.codegym.shopyy.model.ProductDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface IProductDetailRepository extends JpaRepository<ProductDetail, Long> {
     Iterable<ProductDetail> findAllByOrderByPrice();
 
     Iterable<ProductDetail> findAllByPriceBetween(int from, int to);
-
-//    @Query(value = "select * from productDetail order by id desc  limit 4", nativeQuery = true)
-//    Iterable<ProductDetail> findTop4New();
 
 }
