c442cb105a32183bad48a846d072b4d637de763c
HuuTri331 <nguyenhuutri331@gmail.com>
2024-07-08 17:58:16 +0700

    refactor code

--- src/main/java/com/codegym/shopyy/repository/IColorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codegym.shopyy.repository;
-
-import com.codegym.shopyy.model.Color;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IColorRepository extends JpaRepository<Color, Long> {
-}
--- src/main/java/com/codegym/shopyy/repository/ISizeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.codegym.shopyy.repository;
-
-
-import com.codegym.shopyy.model.Size;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ISizeRepository extends JpaRepository<Size, Long> {
-}
