fa51315ccab640d1aa08e58f9d4de767bba8c804
Hoang-An1109 <phamhoangan11092001@gmail.com>
2024-07-09 16:26:07 +0700

    fix security API issue

--- src/main/java/com/codegym/shopyy/model/dto/UserDto.java
+++ src/main/java/com/codegym/shopyy/model/dto/UserDto.java
@@ -1,18 +1,35 @@
 package com.codegym.shopyy.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserDto {
+
     private String fullName;
+
     private String username;
+
     private String password;
+
     private String email;
+
     private String phone;
+
     private String gender;
+
     private Date dob;
+
     private String image;
+
     private String role;
-
-
-
 }
--- src/main/java/com/codegym/shopyy/repository/IUserRepository.java
+++ src/main/java/com/codegym/shopyy/repository/IUserRepository.java
@@ -1,5 +1,6 @@
 package com.codegym.shopyy.repository;
 
+import com.codegym.shopyy.model.entity.Role;
 import com.codegym.shopyy.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -7,18 +8,16 @@ import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface IUserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
-    @Query(nativeQuery = false,
-            value = "select u " +
-                    "from User u " +
-                    "where u.fullName like :fullname")
+    @Query("select u " +
+            "from User u " +
+            "where u.fullName like :fullname")
     List<User> findByFullName(@Param("fullname") String fullname);
 
     @Query(nativeQuery = false,
-            value = "SELECT r.name FROM Role r " +
-                    "INNER JOIN User u ON r.id = u.role.id " +
+            value = "SELECT u.roles FROM User u " +
                     "WHERE u.username = :username")
-    List<String> findRolesByUsername(@Param("username") String username);
+    List<Role> findRolesByUsername(@Param("username") String username);
 }
